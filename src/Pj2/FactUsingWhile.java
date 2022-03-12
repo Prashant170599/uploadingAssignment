b6d718e133ca94da44f3556934299517c7d94179
Prashant Jha <prashant.jha@fulcrumdigital.com>
2022-03-12 22:09:35 +0530

    adding the assignment1

---
+++ src/Pj2/FactUsingWhile.java
@@ -0,0 +1,26 @@
+package Pj2;
+import java.util.Scanner;
+
+public class FactUsingWhile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int fact = 1;  
+	        int i = 1;  
+	  
+	        Scanner sc = new Scanner(System.in);  
+	  
+	        System.out.println("Enter a number: ");  
+	        int num = sc.nextInt();  
+	          
+	        while( i <= num ){  
+	            fact = fact * i;   
+	            i++;
+	        }     
+	  
+	        System.out.println("\nFactorial of " + num + " is: " + fact);  
+	    }  
+	}  
+	
+
+
